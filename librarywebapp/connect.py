581d6ff7f67ffcf0b2b4caa0816fcd265ea908a2
royleon4 <royleon4@gmail.com>
2023-02-01 21:30:26 +1300

    init

--- librarywebapp/connect.py
+++ librarywebapp/connect.py
@@ -4,4 +4,4 @@ dbuser = "royleon4" #PUT YOUR MySQL username here - usually root
 dbpass = "veryhardtoguess" #PUT YOUR PASSWORD HERE
 dbhost = "localhost" 
 dbport = "3306"
-dbname = "library"
+dbname = "royleon4$library"
